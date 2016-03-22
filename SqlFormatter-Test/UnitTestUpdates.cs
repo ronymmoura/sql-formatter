1d3034732e628c4562989929ba9fe6d92d3a92b7
Rony Moura <ronymmoura@gmail.com>
2016-03-22 11:15:35 -0300

    Implementing selects

--- SqlFormatter-Test/UnitTestUpdates.cs
+++ SqlFormatter-Test/UnitTestUpdates.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SqlFormatter;
 
 namespace SqlFormatter_Test
 {
@@ -9,7 +10,31 @@ namespace SqlFormatter_Test
         [TestMethod]
         public void Update()
         {
+            var expected = "UPDATE TEST \n" +
+                           "SET COLUMN_1 = COLUMN_1";
+            var formatter = new Formatter("UPDATE TEST SET COLUMN_1 = COLUMN_1");
+            Assert.AreEqual(expected, formatter.Format());
+        }
 
+        [TestMethod]
+        public void Update_Where()
+        {
+            var expected = "UPDATE TEST \n" +
+                           "SET COLUMN_1 = COLUMN_1\n" +
+                           "WHERE COLUMN_1 = COLUMN_2";
+            var formatter = new Formatter("UPDATE TEST SET COLUMN_1 = COLUMN_1 WHERE COLUMN_1 = COLUMN_2");
+            Assert.AreEqual(expected, formatter.Format());
+        }
+
+        [TestMethod]
+        public void Update_WhereVariable()
+        {
+            var expected = "UPDATE TEST \n" +
+                           "SET COLUMN_1 = @VAR_2\n" +
+                           "WHERE COLUMN_1 = @VAR_1\n" +
+                           "  AND COLUMN_2 = @VAR_2";
+            var formatter = new Formatter("UPDATE TEST SET COLUMN_1 = @VAR_2 WHERE COLUMN_1 = @VAR_1 AND COLUMN_2 = @VAR_2");
+            Assert.AreEqual(expected, formatter.Format());
         }
     }
 }
